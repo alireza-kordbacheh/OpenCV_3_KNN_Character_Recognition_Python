fbfb455c9cf95360ab4b5b99bb28fd52820235e1
alireza-kordbacheh <ar09198779970@yahoo.com>
2023-10-06 00:45:40 +0330

    the 2023 code

--- TrainAndTest.py
+++ TrainAndTest.py
@@ -5,149 +5,112 @@ import numpy as np
 import operator
 import os
 
-# module level variables ##########################################################################
+# Module-level variables
 MIN_CONTOUR_AREA = 100
-
 RESIZED_IMAGE_WIDTH = 20
 RESIZED_IMAGE_HEIGHT = 30
 
-###################################################################################################
 class ContourWithData():
+    # Member variables
+    npaContour = None
+    boundingRect = None
+    intRectX = 0
+    intRectY = 0
+    intRectWidth = 0
+    intRectHeight = 0
+    fltArea = 0.0
 
-    # member variables ############################################################################
-    npaContour = None           # contour
-    boundingRect = None         # bounding rect for contour
-    intRectX = 0                # bounding rect top left corner x location
-    intRectY = 0                # bounding rect top left corner y location
-    intRectWidth = 0            # bounding rect width
-    intRectHeight = 0           # bounding rect height
-    fltArea = 0.0               # area of contour
-
-    def calculateRectTopLeftPointAndWidthAndHeight(self):               # calculate bounding rect info
+    def calculateRectTopLeftPointAndWidthAndHeight(self):
         [intX, intY, intWidth, intHeight] = self.boundingRect
         self.intRectX = intX
         self.intRectY = intY
         self.intRectWidth = intWidth
         self.intRectHeight = intHeight
 
-    def checkIfContourIsValid(self):                            # this is oversimplified, for a production grade program
-        if self.fltArea < MIN_CONTOUR_AREA: return False        # much better validity checking would be necessary
+    def checkIfContourIsValid(self):
+        if self.fltArea < MIN_CONTOUR_AREA:
+            return False
         return True
 
-###################################################################################################
 def main():
-    allContoursWithData = []                # declare empty lists,
-    validContoursWithData = []              # we will fill these shortly
+    allContoursWithData = []
+    validContoursWithData = []
 
     try:
-        npaClassifications = np.loadtxt("classifications.txt", np.float32)                  # read in training classifications
-    except:
-        print "error, unable to open classifications.txt, exiting program\n"
+        npaClassifications = np.loadtxt("classifications.txt", np.float32)
+    except Exception as e:
+        print("Error: Unable to open classifications.txt, exiting program\n")
+        print(e)
         os.system("pause")
         return
-    # end try
 
     try:
-        npaFlattenedImages = np.loadtxt("flattened_images.txt", np.float32)                 # read in training images
-    except:
-        print "error, unable to open flattened_images.txt, exiting program\n"
+        npaFlattenedImages = np.loadtxt("flattened_images.txt", np.float32)
+    except Exception as e:
+        print("Error: Unable to open flattened_images.txt, exiting program\n")
+        print(e)
         os.system("pause")
         return
-    # end try
 
-    npaClassifications = npaClassifications.reshape((npaClassifications.size, 1))       # reshape numpy array to 1d, necessary to pass to call to train
-
-    kNearest = cv2.ml.KNearest_create()                   # instantiate KNN object
-
+    npaClassifications = npaClassifications.reshape((npaClassifications.size, 1))
+    kNearest = cv2.ml.KNearest_create()
     kNearest.train(npaFlattenedImages, cv2.ml.ROW_SAMPLE, npaClassifications)
 
-    imgTestingNumbers = cv2.imread("test1.png")          # read in testing numbers image
+    imgTestingNumbers = cv2.imread("test5.png")
 
-    if imgTestingNumbers is None:                           # if image was not read successfully
-        print "error: image not read from file \n\n"        # print error message to std out
-        os.system("pause")                                  # pause so user can see error message
-        return                                              # and exit function (which exits program)
-    # end if
+    if imgTestingNumbers is None:
+        print("Error: Image not read from file\n")
+        os.system("pause")
+        return
 
-    imgGray = cv2.cvtColor(imgTestingNumbers, cv2.COLOR_BGR2GRAY)       # get grayscale image
-    imgBlurred = cv2.GaussianBlur(imgGray, (5,5), 0)                    # blur
+    imgGray = cv2.cvtColor(imgTestingNumbers, cv2.COLOR_BGR2GRAY)
+    imgBlurred = cv2.GaussianBlur(imgGray, (5, 5), 0)
 
-                                                        # filter image from grayscale to black and white
-    imgThresh = cv2.adaptiveThreshold(imgBlurred,                           # input image
-                                      255,                                  # make pixels that pass the threshold full white
-                                      cv2.ADAPTIVE_THRESH_GAUSSIAN_C,       # use gaussian rather than mean, seems to give better results
-                                      cv2.THRESH_BINARY_INV,                # invert so foreground will be white, background will be black
-                                      11,                                   # size of a pixel neighborhood used to calculate threshold value
-                                      2)                                    # constant subtracted from the mean or weighted mean
+    imgThresh = cv2.adaptiveThreshold(imgBlurred, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+                                      cv2.THRESH_BINARY_INV, 11, 2)
 
-    imgThreshCopy = imgThresh.copy()        # make a copy of the thresh image, this in necessary b/c findContours modifies the image
+    imgThreshCopy = imgThresh.copy()
 
-    imgContours, npaContours, npaHierarchy = cv2.findContours(imgThreshCopy,             # input image, make sure to use a copy since the function will modify this image in the course of finding contours
-                                                 cv2.RETR_EXTERNAL,         # retrieve the outermost contours only
-                                                 cv2.CHAIN_APPROX_SIMPLE)   # compress horizontal, vertical, and diagonal segments and leave only their end points
+    npaContours, _ = cv2.findContours(imgThreshCopy, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-    for npaContour in npaContours:                             # for each contour
-        contourWithData = ContourWithData()                                             # instantiate a contour with data object
-        contourWithData.npaContour = npaContour                                         # assign contour to contour with data
-        contourWithData.boundingRect = cv2.boundingRect(contourWithData.npaContour)     # get the bounding rect
-        contourWithData.calculateRectTopLeftPointAndWidthAndHeight()                    # get bounding rect info
-        contourWithData.fltArea = cv2.contourArea(contourWithData.npaContour)           # calculate the contour area
-        allContoursWithData.append(contourWithData)                                     # add contour with data object to list of all contours with data
-    # end for
+    for npaContour in npaContours:
+        contourWithData = ContourWithData()
+        contourWithData.npaContour = npaContour
+        contourWithData.boundingRect = cv2.boundingRect(contourWithData.npaContour)
+        contourWithData.calculateRectTopLeftPointAndWidthAndHeight()
+        contourWithData.fltArea = cv2.contourArea(contourWithData.npaContour)
+        allContoursWithData.append(contourWithData)
 
-    for contourWithData in allContoursWithData:                 # for all contours
-        if contourWithData.checkIfContourIsValid():             # check if valid
-            validContoursWithData.append(contourWithData)       # if so, append to valid contour list
-        # end if
-    # end for
+    for contourWithData in allContoursWithData:
+        if contourWithData.checkIfContourIsValid():
+            validContoursWithData.append(contourWithData)
 
-    validContoursWithData.sort(key = operator.attrgetter("intRectX"))         # sort contours from left to right
+    validContoursWithData.sort(key=operator.attrgetter("intRectX"))
 
-    strFinalString = ""         # declare final string, this will have the final number sequence by the end of the program
+    strFinalString = ""
 
-    for contourWithData in validContoursWithData:            # for each contour
-                                                # draw a green rect around the current char
-        cv2.rectangle(imgTestingNumbers,                                        # draw rectangle on original testing image
-                      (contourWithData.intRectX, contourWithData.intRectY),     # upper left corner
-                      (contourWithData.intRectX + contourWithData.intRectWidth, contourWithData.intRectY + contourWithData.intRectHeight),      # lower right corner
-                      (0, 255, 0),              # green
-                      2)                        # thickness
+    for contourWithData in validContoursWithData:
+        cv2.rectangle(imgTestingNumbers, (contourWithData.intRectX, contourWithData.intRectY),
+                      (contourWithData.intRectX + contourWithData.intRectWidth,
+                       contourWithData.intRectY + contourWithData.intRectHeight),
+                      (0, 255, 0), 2)
 
-        imgROI = imgThresh[contourWithData.intRectY : contourWithData.intRectY + contourWithData.intRectHeight,     # crop char out of threshold image
-                           contourWithData.intRectX : contourWithData.intRectX + contourWithData.intRectWidth]
+        imgROI = imgThresh[contourWithData.intRectY:contourWithData.intRectY + contourWithData.intRectHeight,
+                 contourWithData.intRectX:contourWithData.intRectX + contourWithData.intRectWidth]
 
-        imgROIResized = cv2.resize(imgROI, (RESIZED_IMAGE_WIDTH, RESIZED_IMAGE_HEIGHT))             # resize image, this will be more consistent for recognition and storage
+        imgROIResized = cv2.resize(imgROI, (RESIZED_IMAGE_WIDTH, RESIZED_IMAGE_HEIGHT))
 
-        npaROIResized = imgROIResized.reshape((1, RESIZED_IMAGE_WIDTH * RESIZED_IMAGE_HEIGHT))      # flatten image into 1d numpy array
+        npaROIResized = imgROIResized.reshape((1, RESIZED_IMAGE_WIDTH * RESIZED_IMAGE_HEIGHT))
+        npaROIResized = np.float32(npaROIResized)
 
-        npaROIResized = np.float32(npaROIResized)       # convert from 1d numpy array of ints to 1d numpy array of floats
+        retval, npaResults, _, _ = kNearest.findNearest(npaROIResized, k=1)
+        strCurrentChar = str(chr(int(npaResults[0][0])))
+        strFinalString = strFinalString + strCurrentChar
 
-        retval, npaResults, neigh_resp, dists = kNearest.findNearest(npaROIResized, k = 1)     # call KNN function find_nearest
+    print("\n" + strFinalString + "\n")
+    cv2.imshow("imgTestingNumbers", imgTestingNumbers)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
-        strCurrentChar = str(chr(int(npaResults[0][0])))                                             # get character from results
-
-        strFinalString = strFinalString + strCurrentChar            # append current char to full string
-    # end for
-
-    print "\n" + strFinalString + "\n"                  # show the full string
-
-    cv2.imshow("imgTestingNumbers", imgTestingNumbers)      # show input image with green boxes drawn around found digits
-    cv2.waitKey(0)                                          # wait for user key press
-
-    cv2.destroyAllWindows()             # remove windows from memory
-
-    return
-
-###################################################################################################
 if __name__ == "__main__":
     main()
-# end if
-
-
-
-
-
-
-
-
-
